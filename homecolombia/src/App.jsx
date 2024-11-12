a3409583159ae07c8d58991049ce464dd2654527
MafeSaavedra <mariafernanda.saavedra@genstudents.org>
2024-11-12 14:31:48 -0500

    Navbar completado

---
+++ homecolombia/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import './main.css';
+import { Navbar } from './Navbar/Navbar.jsx';
+
+export function App() {
+  return (
+    <>
+      <div className="Content-page">
+        <header>
+          <Navbar />
+        </header>
+      </div>
+    </>
+  );
+
+}
---
+++ homecolombia/src/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import './Navbar.css';
+import logoImg from '../img/logohome.svg';
+
+export function Navbar({ logo = logoImg }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => { setIsOpen(!isOpen); };
+
+    return (
+        <div className="Nav-back">
+            <div className="ctn">
+
+                <div className={`navbar-links ${isOpen ? "active" : ""}`} >
+                    <div id="ctn-links">
+                        <ul>
+                            <li><a href="#home.jsx">Inicio</a></li>
+                            <li><a href="#productos.jsx">Contáctanos</a></li>
+                            <li><a href="#contact">Nuestros Proyectos</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+
+                <div className="ctn-logo" id="ctn-logo">
+                    <img className="logohome" src={logo} alt="Logo" />
+                </div>
+
+
+                <div className="hamburger" onClick={toggleMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+
+
+                <div className={`navbar-links ${isOpen ? "active" : ""}`} >
+                    <div id="ctn-links">
+                        <ul>
+                            <li><a href="#lotes">Lotes</a></li>
+                            <li><a href="#ubicacion">Ubicación</a></li>
+                            <li><a href="#acerca">Acerca de Nosotros</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
